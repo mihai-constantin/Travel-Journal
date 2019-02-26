e8d4295ea46884c844b37e48246f1d11e8779c32
mihaiconstantin <mihai.constantin98@gmail.com>
2019-02-26 10:04:24 +0200

    Add theme for LoginActivity

--- app/src/main/java/com/mihaiconstantin/traveljournal/navigationDrawer/NavigationDrawerActivity.java
+++ app/src/main/java/com/mihaiconstantin/traveljournal/navigationDrawer/NavigationDrawerActivity.java
@@ -80,7 +80,7 @@ public class NavigationDrawerActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_exit) {
             return true;
         }
 
